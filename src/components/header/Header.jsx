691625abf443df7c07ab8965e4b4892b94cc0ad4
danielveg4 <dvgilles@gmail.com>
2024-01-22 14:59:32 +0100

    modules

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import { StyledTitle, StyledHeader } from './styles';
+
+const Header = () => {
+	return (
+		<StyledHeader>
+			<StyledTitle>TIENDA DE ADRI</StyledTitle>
+		</StyledHeader>
+	);
+};
+
+export default Header;
---
+++ src/components/header/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.header`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: #282c34;
+    height: 10rem;
+`;
+
+export const StyledTitle = styled.h1`
+    font-size: 3rem;
+    color: white;
+`;
